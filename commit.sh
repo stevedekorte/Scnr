9bc1b593d0357534c94d6779bc0148917a3146c1
Steve Dekorte <steve@dekorte.com>
2015-04-14 11:34:45 -0700

    removing NavNodeKitNew
    Signed-off-by: Steve Dekorte <steve@dekorte.com>

---
+++ commit.sh
@@ -0,0 +1,8 @@
+cd submodules
+cd FoundationCategoriesKit; pwd; git commit . -s; cd ..	
+cd NavNodeKit; pwd; git commit . -s; cd ..	
+cd NavKit; pwd; git commit . -s; cd ..	
+
+cd ..
+pwd; 
+git commit . -s; 
